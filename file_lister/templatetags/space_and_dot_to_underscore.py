82e15544565bc98381fcd9eeb6c204c7196cdeec
Morgan Faget <faget.morgan@gmail.com>
2017-07-22 14:12:25 +0100

    This is finally here you've all been waiting for this

--- file_lister/templatetags/space_and_dot_to_underscore.py
+++ file_lister/templatetags/space_and_dot_to_underscore.py
@@ -4,8 +4,8 @@ import re
 register = template.Library()
 
 
-def space_and_dot_to_underscore(val):
-    return re.sub(r"[ \.]", '_', val)
+def special_to_underscore(val):
+    return re.sub(r"[ \.\,\t\?\!]", '_', val)
 
 
 def remove_brackets_and_plus(val):
@@ -15,6 +15,6 @@ def remove_brackets_and_plus(val):
 def replace_plus_in_url(val):
     return re.sub(r"\+", "%2B", val)
 
-register.filter("space_and_dot_to_underscore", space_and_dot_to_underscore)
+register.filter("special_to_underscore", special_to_underscore)
 register.filter("remove_brackets_and_plus", remove_brackets_and_plus)
 register.filter("replace_plus_in_url", replace_plus_in_url)
